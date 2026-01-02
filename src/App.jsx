abb68d4af9b05e723cb072a95947441d278cb411
Sandesh Pulami <sandeshpulami333@gmail.com>
2026-01-02 03:37:08 +0000

    feat: add a normal ui for the calculator

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import "./App.css";
+export default function App() {
+  return (
+    <>
+
+      <h1>Counter</h1>
+      <div className="card">
+        Count is 37
+      </div>
+      <div>
+        <button
+          onClick={() => { }}
+          style={{ margin: "0 5 px" }}
+        >
+          Increase
+        </button>
+
+        <button
+          onClick={() => { }}
+          style={{ margin: "0 5 px" }}
+        >
+          Decrease
+        </button>
+
+        <button
+          onClick={() => { }}
+          style={{ margin: "0 5 px" }}
+        >
+          Reset
+        </button>
+      </div>
+
+      <div style={{ margin: "10px 0" }}>
+        <input
+          style={{
+            width: "100px",
+            border: "1px solid white",
+            margin: "0 5px",
+            padding: "0.6em 1.2em",
+          }}
+          value={10}
+          onChange={() => { }}
+          type="text"
+        >
+        </input>
+        <button>Set to 10</button>
+      </div>
+    </>
+  );
+}
